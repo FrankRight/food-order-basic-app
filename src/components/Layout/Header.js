dcc0180f0f4e8510f288f1f3867b7b4787fb42eb
FrankRight <frankbosire2017@gmail.com>
2022-06-11 17:58:29 +0300

    add header to app

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,16 @@
 import { Fragment } from "react";
-import styles from "./Header";
+import styles from "./Header.module.css";
 
 import meals from "../../assets/meals.jpg";
 
 function Header(props) {
   return (
     <Fragment>
-      <header>
+      <header className={styles.header}>
         <h1>RightMeals</h1>
         <button>Cart</button>
       </header>
-      <div>
+      <div className={styles["main-image"]}>
         <img src={meals} alt="a table with nice food" />
       </div>
     </Fragment>
